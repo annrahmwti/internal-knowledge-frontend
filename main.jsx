c8238696d48c94cdf8f5f3c376f61c5913525809
annrahmwti <101492462+annrahmwti@users.noreply.github.com>
2026-06-11 12:25:35 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GonsaveChatbot from "./GonsaveChatbot";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GonsaveChatbot />
+  </React.StrictMode>
+);
